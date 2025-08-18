908c94901c05b9a2cacd91de4cde3c19905b28dd
Marcin Żołubowski <marcin.zolubowski@gmail.com>
2025-08-18 23:03:15 +0200

    Added qt server
    Added working Qt Echo server

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ REM Enter "build" directory
 cd build
 
 REM Cmake ..
-cmake .. -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Debug
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=C:\Qt\6.9.1\msvc2022_64
 
 REM Build solution(/v:diag for verbose)
 msbuild WebSocket-Echo.sln -m
---
+++ server/qt_server.cpp
@@ -0,0 +1,57 @@
+#include <QCoreApplication>
+#include <QWebSocketServer>
+#include <QWebSocket>
+#include <QDebug>
+
+///
+/// Run exe with qt cmd
+///
+
+class echo_server : public QObject
+{
+    Q_OBJECT
+public:
+    echo_server(const quint16 port, QObject* parent = nullptr)
+    : QObject(parent), server_("WebSocket Echo Server", QWebSocketServer::NonSecureMode, this)
+    {
+        if (server_.listen(QHostAddress::Any, port))
+        {
+            qDebug() << "Listening on port " << port;
+            connect(&server_, &QWebSocketServer::newConnection, this, &echo_server::on_new_connection);
+        }
+    }
+
+private Q_SLOTS:
+    void on_new_connection()
+    {
+        QWebSocket *socket = server_.nextPendingConnection();
+        connect(socket, &QWebSocket::textMessageReceived, this, &echo_server::on_message);
+        connect(socket, &QWebSocket::disconnected, this, &echo_server::on_disconnected);
+    }
+
+    void on_message(const QString& message) const
+    {
+        QWebSocket* client = qobject_cast<QWebSocket*>(sender());
+        client->sendTextMessage(message);
+    }
+
+    void on_disconnected()
+    {
+        QWebSocket *client = qobject_cast<QWebSocket *>(sender());
+        client->deleteLater();
+    }
+
+private:
+    QWebSocketServer server_;
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    echo_server echo_server(9001);
+
+    return a.exec();
+}
+
+#include "qt_server.moc"
